975061c98b54e26f2866d3bccac53dd004c9c0c5
Hang Zhou <zhou_hang@berkeley.edu>
2023-04-26 14:54:35 -0400

    test activate

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 python3 -m venv venv && source venv/bin/activate && pip install -r ./requirements.txt
-
+activate() {
+    source venv/bin/activate
+}
 echo "############################"
 echo "Please be sure to install your openai api key!"
 echo "For the terminal windows, resize it to be at least 120 wide and 30 height"
